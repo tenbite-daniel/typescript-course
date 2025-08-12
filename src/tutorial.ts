881413c61a6681138ee198189baad58220c79cab
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-12 18:58:09 +0300

    Completed Intro, Type Annotations, Arrays

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,6 +1,21 @@
-let orderStatus: "processing" | "shipped" | "delivered" = "processing";
-orderStatus = "shipped";
-orderStatus = "delivered";
+let prices: number[] = [100, 75, 42];
+// prices.push('hello')
 
-let discount: number | string = 20;
-discount = "20%";
+let fruit: string[] = ["apple", "orange"];
+
+// let randomValues:[] = ['helo']
+
+let emptyValues: number[] = [];
+
+let names = ["peter", "susan", 1];
+let array: (string | boolean)[] = ["apple", true, "orange", false];
+
+// Array Challange
+
+let temperatures: number[] = [20, 25, 30];
+// temperatures.push('hot')
+
+let colors: string[] = ["red", "green", "blue"];
+// colors.push(true)
+
+let mixedArray: (number | string)[] = [1, "two", 3];
