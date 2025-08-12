42e99023576fc0b4ecfd3592c89be84e68a07bd8
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-12 14:28:19 +0300

    Intro, Type Annotations

---
+++ src/tutorial.ts
@@ -0,0 +1,13 @@
+let awesomeName: string = "shakeAndBake";
+awesomeName = "something";
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+awesomeName = 20;
+
+let amount: number = 20;
+amount = 12 - 1;
+// amount = "pants";
+
+let isAwesome: boolean = true;
+isAwesome = false;
+// isAwesome = "something";
