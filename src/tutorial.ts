96a216a679359ce10d8bac18aab468ecf8546a14
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-13 22:08:55 +0300

    feat: type alias intersection type in union and computed properties

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -176,27 +176,54 @@
 // }
 // setTheme("dark");
 
-// challange
-type Employee = { id: number; name: string; department: string };
-type Manager = { id: number; name: string; employees: Employee[] };
-type Staff = Employee | Manager;
+// // challange
+// type Employee = { id: number; name: string; department: string };
+// type Manager = { id: number; name: string; employees: Employee[] };
+// type Staff = Employee | Manager;
 
-function printStaffDetails(staff: Staff): void {
-    if ("employees" in staff) {
-        console.log(
-            `${staff.name} is a manager of ${staff.employees.length} employees`
-        );
-    } else {
-        console.log(
-            `${staff.name} is an employee in the ${staff.department} department`
-        );
-    }
-}
+// function printStaffDetails(staff: Staff): void {
+//     if ("employees" in staff) {
+//         console.log(
+//             `${staff.name} is a manager of ${staff.employees.length} employees`
+//         );
+//     } else {
+//         console.log(
+//             `${staff.name} is an employee in the ${staff.department} department`
+//         );
+//     }
+// }
 
-const alice: Employee = { id: 1, name: "alice", department: "Sales" };
-const steve: Employee = { id: 2, name: "steve", department: "HR" };
-const bob: Manager = { id: 1, name: "bobo", employees: [alice, steve] };
+// const alice: Employee = { id: 1, name: "alice", department: "Sales" };
+// const steve: Employee = { id: 2, name: "steve", department: "HR" };
+// const bob: Manager = { id: 1, name: "bobo", employees: [alice, steve] };
 
-printStaffDetails(alice);
-printStaffDetails(bob);
-printStaffDetails(steve);
+// printStaffDetails(alice);
+// printStaffDetails(bob);
+// printStaffDetails(steve);
+
+// the intersection type in Union
+type Book = { id: number; name: string; price: number };
+type DiscountedBook = Book & { discount: number };
+const book1: Book = {
+    id: 1,
+    name: "how to cook a dragon",
+    price: 15,
+};
+const book2: Book = {
+    id: 2,
+    name: "the secret life of unicorns",
+    price: 18,
+};
+const discountedBook: DiscountedBook = {
+    id: 2,
+    name: "Gnomes vs. Goblins: The Ultimate Guide",
+    price: 25,
+    discount: 0.15,
+};
+
+// type alias also supports computed properties
+const propName = "age";
+type Animal = {
+    [propName]: number;
+};
+let tiger: Animal = { [propName]: 5 };
