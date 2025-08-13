6c0a98784e7f19e074dde88f28c9c6312f0b6033
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-13 22:00:24 +0300

    chore: completed type alias challange

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -142,36 +142,61 @@
 // console.log(processData("hello"));
 // console.log(processData("hello", { reverse: true }));
 
-// Type Alias
-type User = { id: number; name: string; isActive: boolean };
-const john: User = {
-    id: 1,
-    name: "john",
-    isActive: true,
-};
-const susan: User = {
-    id: 1,
-    name: "susan",
-    isActive: false,
-};
+// // Type Alias
+// type User = { id: number; name: string; isActive: boolean };
+// const john: User = {
+//     id: 1,
+//     name: "john",
+//     isActive: true,
+// };
+// const susan: User = {
+//     id: 1,
+//     name: "susan",
+//     isActive: false,
+// };
 
-function createUser(user: User): User {
-    console.log(`Hello there ${user.name.toUpperCase()} !!!`);
-    return user;
+// function createUser(user: User): User {
+//     console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+//     return user;
+// }
+
+// type StringOrNumber = string | number;
+// let value: StringOrNumber;
+// value = "hello";
+// value = 123;
+
+// type Theme = "light" | "dark";
+
+// let theme: Theme;
+// theme = "dark";
+// theme = "light";
+
+// function setTheme(t: Theme) {
+//     theme = t;
+// }
+// setTheme("dark");
+
+// challange
+type Employee = { id: number; name: string; department: string };
+type Manager = { id: number; name: string; employees: Employee[] };
+type Staff = Employee | Manager;
+
+function printStaffDetails(staff: Staff): void {
+    if ("employees" in staff) {
+        console.log(
+            `${staff.name} is a manager of ${staff.employees.length} employees`
+        );
+    } else {
+        console.log(
+            `${staff.name} is an employee in the ${staff.department} department`
+        );
+    }
 }
 
-type StringOrNumber = string | number;
-let value: StringOrNumber;
-value = "hello";
-value = 123;
+const alice: Employee = { id: 1, name: "alice", department: "Sales" };
+const steve: Employee = { id: 2, name: "steve", department: "HR" };
+const bob: Manager = { id: 1, name: "bobo", employees: [alice, steve] };
 
-type Theme = "light" | "dark";
-
-let theme: Theme;
-theme = "dark";
-theme = "light";
-
-function setTheme(t: Theme) {
-    theme = t;
-}
-setTheme("dark");
+printStaffDetails(alice);
+printStaffDetails(bob);
+printStaffDetails(steve);
