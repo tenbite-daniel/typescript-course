6f6191fcf8fd910495179cc8ebe47b9907a429a8
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-13 14:21:07 +0300

    feat: complete lesson on rest parameter

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -41,20 +41,33 @@
 // }
 
 // optional and default parameters in functions
-//optional parameter
-function calculatePrice(price: number, discount?: number): number {
-    return price - (discount || 0);
+
+// //optional parameter
+// function calculatePrice(price: number, discount?: number): number {
+//     return price - (discount || 0);
+// }
+
+// let priceAfterDiscount = calculatePrice(100, 20);
+
+// // default parameter
+// function calculateScore(
+//     initialScore: number,
+//     penaltyPoints: number = 0
+// ): number {
+//     return initialScore - penaltyPoints;
+// }
+
+// let scoreAfterPenalty = calculateScore(100, 20);
+// let scoreWithoutPenalty = calculateScore(300);
+
+// rest parameter
+function sum(message: string, ...numbers: number[]): string {
+    const doubled = numbers.map((num) => num * 2);
+    console.log(doubled);
+    let total = numbers.reduce((prev, current) => {
+        return prev + current;
+    }, 0);
+    return `${message} ${total}`;
 }
-
-let priceAfterDiscount = calculatePrice(100, 20);
-
-// default parameter
-function calculateScore(
-    initialScore: number,
-    penaltyPoints: number = 0
-): number {
-    return initialScore - penaltyPoints;
-}
-
-let scoreAfterPenalty = calculateScore(100, 20);
-let scoreWithoutPenalty = calculateScore(300);
+let result = sum("The total is: ", 1, 2, 3, 4, 5);
+console.log(result);
