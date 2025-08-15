39767ea3e2b019a9958350064b4ce1d8bd0d5e9c
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-15 13:48:55 +0300

    feat: completed lesson on zod library for validation data at run time

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -866,16 +866,20 @@
 // };
 
 // Fetch Data
+import { z } from "zod";
+
 const url = "https://www.course-api.com/react-tours-project";
 
-type Tour = {
-    id: string;
-    name: string;
-    info: string;
-    image: string;
-    price: string;
-    something: boolean;
-};
+const tourSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    info: z.string(),
+    image: z.string(),
+    price: z.string(),
+    something: z.string(),
+});
+
+type Tour = z.infer<typeof tourSchema>;
 
 async function fetchData(url: string): Promise<Tour[]> {
     try {
@@ -883,10 +887,15 @@ async function fetchData(url: string): Promise<Tour[]> {
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
-        const data: Tour[] = await response.json();
-        console.log(data);
+        const rawData: Tour[] = await response.json();
 
-        return data;
+        const result = tourSchema.array().safeParse(rawData);
+        if (!result.success) {
+            throw new Error(`Invalid data: ${result.error}`);
+        }
+        console.log(result);
+
+        return result.data;
     } catch (error) {
         const errMsg =
             error instanceof Error ? error.message : "there was an error";
@@ -897,5 +906,5 @@ async function fetchData(url: string): Promise<Tour[]> {
 
 const tours = await fetchData(url);
 tours.map((tour) => {
-    console.log(tour.something);
+    console.log(tour.name);
 });
