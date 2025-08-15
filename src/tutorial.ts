cdd569b530d74c5ae0eeb59a0c1c2b7d9d574cd3
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-15 12:42:13 +0300

    feat: completed lesson on Generics - Default value

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -778,38 +778,89 @@
 
 // const result = someFunc();
 
-// Generics - Generate Array
+// // Generics - Generate Array
 
-function generateStringArray(length: number, value: string): string[] {
-    let result: string[] = [];
-    result = Array(length).fill(value);
-    return result;
+// function generateStringArray(length: number, value: string): string[] {
+//     let result: string[] = [];
+//     result = Array(length).fill(value);
+//     return result;
+// }
+// console.log(generateStringArray(3, "hi"));
+
+// function createArray<T>(length: number, value: T): Array<T> {
+//     let result: T[] = [];
+//     result = Array(length).fill(value);
+//     return result;
+// }
+
+// let arrayStrings = createArray<string>(10, "hi");
+// let arrayNumbers = createArray<number>(10, 100);
+// console.log(arrayStrings);
+// console.log(arrayNumbers);
+
+// // multiple variable types
+// function pair<T, U>(param1: T, param2: U): [T, U] {
+//     return [param1, param2];
+// }
+// let result = pair<number, string>(123, "hi");
+// console.log(result);
+
+// function processValue<T extends string | number>(value: T): T {
+//     console.log(value);
+//     return value;
+// }
+
+// processValue("hello");
+// processValue(123);
+// // processValue(true);
+
+// ## Generics - Type Constraints 2
+
+type Car = {
+    brand: string;
+    model: string;
+};
+
+const car: Car = {
+    brand: "ford",
+    model: "mustang",
+};
+
+type Product = {
+    name: string;
+    price: number;
+};
+
+const product: Product = {
+    name: "shoes",
+    price: 1.99,
+};
+
+type Student = {
+    name: string;
+    age: number;
+};
+
+const student: Student = {
+    name: "peter",
+    age: 20,
+};
+
+function printName<T extends { name: string }>(input: T): void {
+    console.log(input.name);
 }
-console.log(generateStringArray(3, "hi"));
+printName(student);
+printName(product);
+// printName(car);
 
-function createArray<T>(length: number, value: T): Array<T> {
-    let result: T[] = [];
-    result = Array(length).fill(value);
-    return result;
+// Generics - Default Value
+interface StoreData<T = any> {
+    data: T[];
 }
+const storeNumbers: StoreData<number> = {
+    data: [1, 2, 3, 4, 5],
+};
 
-let arrayStrings = createArray<string>(10, "hi");
-let arrayNumbers = createArray<number>(10, 100);
-console.log(arrayStrings);
-console.log(arrayNumbers);
-
-// multiple variable types
-function pair<T, U>(param1: T, param2: U): [T, U] {
-    return [param1, param2];
-}
-let result = pair<number, string>(123, "hi");
-console.log(result);
-
-function processValue<T extends string | number>(value: T): T {
-    console.log(value);
-    return value;
-}
-
-processValue("hello");
-processValue(123);
-// processValue(true);
+const randomStuff: StoreData = {
+    data: ["random", 21],
+};
