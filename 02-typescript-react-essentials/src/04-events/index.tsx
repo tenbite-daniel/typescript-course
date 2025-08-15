268460f76165c0c9fc06b374416d21d90b51f867
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-16 00:39:05 +0300

    feat: completed lesson on events

--- 02-typescript-react-essentials/src/04-events/index.tsx
+++ 02-typescript-react-essentials/src/04-events/index.tsx
@@ -1,9 +1,52 @@
+import { useState } from "react";
+
+type Person = {
+    name: string;
+};
+
 function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
-  );
+    const [text, setText] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // console.log(e.target.value);
+        setEmail(e.target.value);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        // const formData = new FormData(e.target as HTMLFormElement);
+        const data = Object.fromEntries(formData);
+        console.log(data);
+
+        const text = formData.get("text") as string;
+        const person: Person = { name: data.text as string };
+    };
+
+    return (
+        <section>
+            <h2>event example</h2>
+            <form onSubmit={handleSubmit} className="form">
+                <input
+                    type="text"
+                    className="form-input mb-1"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                    name="text"
+                />
+                <input
+                    type="email"
+                    className="form-input mb-1"
+                    value={email}
+                    onChange={handleChange}
+                    name="email"
+                />
+                <button type="submit" className="btn btn-block">
+                    Submit
+                </button>
+            </form>
+        </section>
+    );
 }
 export default Component;
