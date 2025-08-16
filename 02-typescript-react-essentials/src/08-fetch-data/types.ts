85e74a3b7865ae004d3dd9fba0858d190afb0cb0
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-16 14:04:59 +0300

    feat: completed lesson on fetch data using the fetch api

---
+++ 02-typescript-react-essentials/src/08-fetch-data/types.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const tourSchema = z.object({
+    id: z.string(),
+    name: z.string(),
+    info: z.string(),
+    image: z.string(),
+    price: z.string(),
+    // something: z.string(),
+});
+
+export type Tour = z.infer<typeof tourSchema>;
