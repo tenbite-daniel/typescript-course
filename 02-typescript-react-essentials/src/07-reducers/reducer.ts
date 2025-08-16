3560404c8c2b85fbcb16dbea951fdf6a36a57ca9
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-08-16 13:41:45 +0300

    feat: completed lesson on reducers

---
+++ 02-typescript-react-essentials/src/07-reducers/reducer.ts
@@ -0,0 +1,43 @@
+export type CounterState = {
+    count: number;
+    status: string;
+};
+
+export const initialState: CounterState = {
+    count: 0,
+    status: "pending",
+};
+
+type UpdateCountAction = {
+    type: "increment" | "decrement" | "reset";
+};
+
+type SetStatusAction = {
+    type: "setStatus" | "des";
+    payload: "active" | "inactive";
+};
+
+type CounterAction = UpdateCountAction | SetStatusAction;
+
+export const counterReducer = (
+    state: CounterState,
+    action: CounterAction
+): CounterState => {
+    switch (action.type) {
+        case "increment":
+            return { ...state, count: state.count + 1 };
+        case "decrement":
+            return { ...state, count: state.count - 1 };
+        case "reset":
+            return { ...state, count: 0 };
+        case "setStatus":
+            return { ...state, status: action.payload };
+        default:
+            const unhandledActionType: never = action;
+            throw new Error(
+                `Unexpected action type: ${unhandledActionType}. Please double check the counter reducer.`
+            );
+
+            return state;
+    }
+};
